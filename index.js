3f261302525dae588071d5dfa4e5c8a80382ddf7
jakemstar <jscob1234@gmail.com>
2021-10-17 19:01:47 -0500

    works

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
+      io.emit('chat message', msg);
+  });
 });
 
 server.listen(3000, () => {
