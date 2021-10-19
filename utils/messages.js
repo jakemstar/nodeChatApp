8d03ab7c8b44dc890e9b2d49a181967c8dd4d785
jakemstar <jscob1234@gmail.com>
2021-10-18 22:52:09 -0500

    added emotes

--- utils/messages.js
+++ utils/messages.js
@@ -1,6 +1,17 @@
 const moment = require('moment');
 
+const emotes = ["slep", "slurp", "1rock", "4real", "bananaeat", "BloodOfTheUnforgiven", "BUP", "CharmedChamp", "ColoraptorApproved", 
+"ColoraptorDisapproved", "crunk", "ducksanyu", "flatchamp", "gayvery", "glasses", "haha", "hahahafuckyou", "hungry", "jacob", 
+"KirbyNoApprove", "KirbySaysTimeToDie", "kiwi","KpopAnger", "meet", "flatvery", "princecharming", "rasim", "SamuraiAnger", "SamuraiApproved", 
+"superior", "suplilbitch"];
+
+function handleEmotes(message){
+    emotes.forEach(emote => message = message.replace(new RegExp(`:${emote}:`, 'g'), `<img src="emotes/${emote}.png" height="50" width="50">`));
+    return message;
+}
+
 function formatMessage(username, message){
+    message = handleEmotes(message);
     return{
         username,
         message,
