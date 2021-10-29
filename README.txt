c263c832a3490365406569d3db24ad5bc3c76223
jakemstar <jscob1234@gmail.com>
2021-10-28 22:41:00 -0500

    Added youtube embeds and mongoDB for storing message history

--- README.txt
+++ README.txt
@@ -7,3 +7,7 @@ Then run
 node server.js
 
 This will start the app on your localhost:3000
+
+Project now implements mongoDB using a collection called Messages and a document called Messages
+You can use your own database for this by creating a free mongoDB cluster to reference and creating
+a collection and document both called Messages
--- server.js
+++ server.js
@@ -7,20 +7,62 @@ const io = new Server(server);
 const path = require('path');
 const formatMessage = require('./utils/messages');
 const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
+const { MongoClient } = require('mongodb');
 
-const messageHistory = [];
+const uri = "redacted"
+const client = new MongoClient(uri);
+
+async function dataBaseStuff(){
+  try{
+    await client.connect();
+  }
+  catch {
+    console.log("Database connnection failed");
+  }
+}
+
+async function createMessage(client, message){
+  try{
+    await client.db("Messages").collection("Messages").insertOne(message);
+  }
+  catch{
+    console.log("Message not inserted");
+  }
+}
+
+async function getHistory(client, room){
+  try{
+    const cursor = client.db("Messages").collection("Messages").find({
+      messageRoom: room
+    }).limit(50)
+
+    const result = await cursor.toArray();
+    return result;
+  }
+  catch{
+    console.log("Failed to obtain message history");
+  }
+}
+
+dataBaseStuff();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-  socket.on('joinRoom', ({username, room}) => {
-    const user = userJoin(socket.id, username, room);
-
+  socket.on('joinRoom', async ({username, room}) => {
+    try{
+    const messageHistory = await getHistory(client, room);
     socket.emit('previous messages', { messageHistory, room });
+    }
+    catch{
+      console.log("Message history fucked up");
+    }
+
+    const user = userJoin(socket.id, username, room);
 
     socket.join(user.room);
 
-    socket.broadcast.to(user.room).emit('chat message', formatMessage('Server', `${user.username} joined`));
+    // socket.broadcast.to(user.room).emit('chat message', formatMessage('Server', `${user.username} joined`));
 
     io.to(user.room).emit('roomUsers', {
       room: user.room,
@@ -30,14 +72,14 @@ io.on('connection', (socket) => {
 
   socket.on('chat message', (msg) => {
     const user = getCurrentUser(socket.id);
-    messageHistory.push({ ...formatMessage(user.username, msg), messageRoom : user.room });
+    createMessage(client, { ...formatMessage(user.username, msg), messageRoom : user.room });
     io.to(user.room).emit('chat message', formatMessage(user.username, msg));
   });
 
   socket.on('disconnect', () => {
     const user = userLeave(socket.id);
     if (user){
-      io.to(user.room).emit('chat message', formatMessage('Server', `${user.username} left`));
+      // io.to(user.room).emit('chat message', formatMessage('Server', `${user.username} left`));
       io.to(user.room).emit('roomUsers', {
         room: user.room,
         users: getRoomUsers(user.room)
